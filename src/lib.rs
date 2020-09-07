a347153748fa212a2af610e9f01e5600b6119999
Mendelt Siebenga <msiebenga@gmail.com>
2020-09-07 21:17:12 +0200

    first try write and write hint

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+use std::io::{Read, Write};
+
+pub struct Trem<R, W> {
+    read: R,
+    write: W,
+}
+
+impl<R: Read, W: Write> Trem<R, W> {
+    pub fn new(read: R, write: W) -> Self {
+        Self {read, write}
+    }
+
+    /// Write a buffer to the output
+    pub fn write(&mut self, buf: &[u8]) {
+        self.write.write(buf).unwrap();
+    }
+
+    /// Write a buffer to the output (maybe in a different colour) and move the cursor back
+    pub fn write_hint(&mut self, buf: &[u8]) {
+        let chars = self.write.write(buf).unwrap();
+        self.write.write(&[0x18, 0x5b, chars as u8]).unwrap();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
